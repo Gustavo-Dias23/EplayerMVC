81f0debe4fcf4fe02e288554bd4fa353a096becd
Gustavo Dias <dias.gustavo23@gmail.com>
2020-07-15 09:57:15 -0300

    Parte 4 e 5 feita

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Aula37EPlayer.Models;
 using Microsoft.AspNetCore.Http;
+using System.IO;
 
 namespace Aula37EPlayer.Controllers
 {
@@ -19,16 +20,46 @@ namespace Aula37EPlayer.Controllers
             return View();
         }
 
-        public IActionResult Cadastrar(IFormCollection form){
+        public IActionResult Cadastrar(IFormCollection form)
+        {
             
             Equipe novaEquipe = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse( form["IdEquipe"] );
             novaEquipe.Nome = form["Nome"];
             novaEquipe.Imagem = form["Imagem"];
 
+            // Upload Início
+            var file    = form.Files[0];
+            var folder  = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/Equipes");
+
+            if(file != null)
+            {
+                if(!Directory.Exists(folder)){
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+                using (var stream = new FileStream(path, FileMode.Create))  
+                {  
+                    file.CopyTo(stream);  
+                }
+                novaEquipe.Imagem   = file.FileName;
+            }
+            else
+            {
+                novaEquipe.Imagem   = "padrao.png";
+            }
+            // Upload Final
+        
             equipeModel.Create(novaEquipe);
 
             ViewBag.Equipes = equipeModel.ReadAll();
+            return LocalRedirect("~/Equipe");
+        }
+
+        [Route("{id}")]
+        public IActionResult Excluir(int id){
+            equipeModel.Delete(id);
             return LocalRedirect("~/Equipe");
         }
     }
